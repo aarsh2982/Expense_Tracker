a2c3ab6e2375c1edbd9f2e5b7c4f4ddf17468429
aarsh2982 <invinciblearjuna1@gmail.com>
2024-01-08 20:02:16 +0530

    Initial commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+    <h4 style="
+      text-align: center;
+      color: #015a9e;
+      font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+      font-size: 14px;
+    ">
+        &copy;2024 Created By Aarsh
+    </h4>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,3 @@
+<template>
+    <h2 style="text-align: center; color: #015a9e; font-size: 34px">Expense Tracker</h2>
+</template>
